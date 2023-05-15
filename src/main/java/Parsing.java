9058417a2db64b634addc828cf9bc473e055608d
@Eliott-b <eliottb.info@gmail.com>
2023-05-15 16:34:52 +0200

    Parsing.java : add method for Parsing scenario file add method for parsing quest line

--- src/main/java/Parsing.java
+++ src/main/java/Parsing.java
@@ -1,20 +1,93 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
 
 /**
  * Classe pour récupérer les données d'un fichier
  * @author Eliott-B, Chakib
  * @see Quest
  * @see Scenario
- * @see File
+ * @see java.util.Scanner
+ * @see java.io.File
+ * @see java.io.FileNotFoundException
  */
 public class Parsing {
 
-     /**
-      * Sépare les données du fichier donné en paramètre et créer des scénarios.
-      * @param file File
-      * @return Scenario
-      */
-     public static Scenario parsing(File file){
-          return new Scenario();
-     }
+    /**
+     * Sépare les données du fichier donné en paramètre et créer des scénarios.
+     * @param file File
+     * @return Scenario
+     * @see Scenario
+     * @see java.util.Scanner
+     * @see java.io.File
+     * @see java.io.FileNotFoundException
+     */
+    public static Scenario parsing(File file) throws FileNotFoundException{
+        Scenario scenario = new Scenario();
+        Scanner scan = new Scanner(file);
+        while(scan.hasNext()){
+            // Ajout de la quête, formée dans la méthode questParsing, dans le scénario.
+            scenario.addQuest(questParsing(scan.nextLine()));
+        }
+        return scenario;
+    }
+
+    /**
+     * Récupère la quête depuis la ligne donnée en paramètre
+     * @param line String
+     * @return Quest
+     * @see Quest
+     */
+    private static Quest questParsing(String line){
+        // 1|(4, 3)|()|2|100|explorer pic de Bhanborim
+        String[] data = line.split("\\|");
+
+        // ID
+        int id = Integer.parseInt(data[0]);
+
+        // POSITION
+        data[1] = data[1].replace(" ", "");
+
+        String[] coordsstring = data[1].replace("(", "").replace(")", "").split(",");
+        Integer[] coords = {Integer.parseInt(coordsstring[0]), Integer.parseInt(coordsstring[1])};
+
+        // PRECONDITION
+        data[2] = data[2].replaceAll(" ","");
+        Integer[][] precondition = new Integer[2][2];
+        if(!Objects.equals(data[2], "()")) {
+            String[] precond = data[2].split("\\),");
+            String[] precondx = precond[0].replaceAll("\\(", "").split(",");
+            String[] precondy = precond[1].replace("(", "").replaceAll("\\)", "").split(",");
+
+            if (precondx.length == 2) {
+                if (precondx[1].length() != 0) {
+                    precondition[0][1] = Integer.parseInt(precondx[1]);
+                }
+            }
+            if (precondx[0].length() != 0) {
+                precondition[0][0] = Integer.parseInt(precondx[0]);
+            }
+
+            if(Objects.equals(precondy.length, 2)) {
+                if (precondy[1].length() != 0) {
+                    precondition[0][1] = Integer.parseInt(precondy[1]);
+                }
+            }
+            if (precondy[0].length() != 0) {
+                precondition[0][0] = Integer.parseInt(precondy[0]);
+            }
+        }
+
+        // DURATION
+        int duration = Integer.parseInt(data[3]);
+
+        // XP
+        int xp = Integer.parseInt(data[4]);
+
+        // TITLE
+        String title = data[5];
+        return new Quest(id, coords, precondition, duration, xp, title);
+    }
 }
