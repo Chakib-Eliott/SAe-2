271d45d1a23dd2d5e00e9f156f20ff4f9658be70
@444chak <@eliott-b>
2023-05-15 10:32:43 +0200

    Add Scenario class with fields

---
+++ src/main/java/Scenario.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+
+/**
+ * Classe des scénarios
+ * @author Eliott-B, Chakib
+ * @see Quest
+ * @see java.util.TreeSet
+ */
+public class Scenario {
+    /**
+     * TreeSet de qui contient des objets de la classe Quest
+     */
+    private TreeSet <Quest> quest;
+}
