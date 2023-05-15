c5339db86178709eea58dcfecfb5b48317ae126e
Chak <444chak@gmail.com>
2023-05-15 17:46:34 +0200

    Map.java : add constructor add toString method for show map in terminal

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,9 +1,61 @@
+import java.util.Iterator;
+
 /**
  * Class de la carte sur laquelle évolue le joueur
  * @author chak, Eliott-B
  * @see Quest
  * @see Scenario
+ * @see java.util.TreeSet
  */
 public class Map {
+    Object[][] map;
 
+    /**
+     * Constructeur de la classe Map
+     * @param scenario Scenario
+     * @see Scenario
+     */
+    public Map(Scenario scenario){
+        Iterator<Quest> iterator = scenario.getQuests().iterator();
+        int maxX = 0;
+        int maxY = 0;
+        while(iterator.hasNext()) {
+            Quest quest = iterator.next();
+            if (quest.getPosition()[0] > maxX) {
+                maxX = quest.getPosition()[0];
+            }
+            if (quest.getPosition()[1] > maxY) {
+                maxY = quest.getPosition()[1];
+            }
+        }
+        map = new Object[maxX+1][maxY+1];
+        iterator = scenario.getQuests().iterator();
+        while(iterator.hasNext()) {
+            Quest quest = iterator.next();
+            map[quest.getPosition()[0]][quest.getPosition()[1]] = quest;
+        }
+    }
+
+    /**
+     * Affichage de la carte dans le terminal, cases séparées par des virgules, les quêtes sont affichés par leurs iD
+     * @return String
+     */
+    public String toString() {
+        StringBuilder tostring = new StringBuilder();
+        for (Object[] objects : map) {
+            for (int j = 0; j < objects.length - 1; j++) {
+                if (objects[j] == null) {
+                    tostring.append("x,");
+                } else {
+                    tostring.append(((Quest) objects[j]).getId()).append(",");
+                }
+            }
+            if (objects[objects.length - 1] == null) {
+                tostring.append("x\n");
+            } else {
+                tostring.append(((Quest) objects[objects.length - 1]).getId()).append("\n");
+            }
+        }
+        return tostring.toString();
+    }
 }
