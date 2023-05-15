b53e03ced3ffbc3f3490b2cdcc19ef5a1cedbcbb
@Eliott-b <eliottb.info@gmail.com>
2023-05-15 10:39:38 +0200

    Add Scenario's constructor

--- src/main/java/Scenario.java
+++ src/main/java/Scenario.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 /**
@@ -5,10 +6,20 @@ import java.util.TreeSet;
  * @author Eliott-B, Chakib
  * @see Quest
  * @see java.util.TreeSet
+ * @see java.util.ArrayList
  */
 public class Scenario {
     /**
      * TreeSet de qui contient des objets de la classe Quest
      */
     private TreeSet <Quest> quest;
+
+    /**
+     * Constructeur de la classe. Instancie le TreeSet.
+     * @param list ArrayList
+     * @see java.util.ArrayList
+     */
+    public Scenario (ArrayList <Quest> list){
+        quest = new TreeSet<>(list);
+    }
 }
