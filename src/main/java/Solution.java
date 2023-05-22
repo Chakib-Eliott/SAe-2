fadc1a00f58f4aa071299ac43b3c4e220b42c6a4
Chak <444chak@gmail.com>
2023-05-22 15:40:23 +0200

    add Solution.java class on which all solutions will be based

---
+++ src/main/java/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Class qui implémente une solution à un scénario
+ * @author chak
+ * @see Scenario
+ */
+public class Solution {
+    Scenario scenario;
+
+    /**
+     * Constructeur de la classe Solution
+     * @param scenario Scenario
+     * @see Scenario
+     */
+    public Solution(Scenario scenario){
+        this.scenario = scenario;}
+}
