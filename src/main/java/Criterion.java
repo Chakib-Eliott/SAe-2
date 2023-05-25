8a46bc3fca49d7725ea0c5015d741be45988c96d
Eliott-B <eliottb.info@gmail.com>
2023-05-25 16:27:32 +0200

    Add method (addCriterion) Add criterion to the object with the solution, the type and the number

--- src/main/java/Criterion.java
+++ src/main/java/Criterion.java
@@ -20,5 +20,27 @@ public class Criterion {
         solutions = new TreeMap<>();
     }
 
-
+    /**
+     * Ajoute un critère au TreeMap.
+     * (solution, meilleur ou pire, nombre de fois)
+     * @param solution String
+     * @param type boolean
+     * @param number int
+     */
+    public void addCriterion(String solution, boolean type, int number){
+        // Vérifie si la solution n'existe pas déjà
+        if(solutions.get(solution) == null){
+            TreeMap <Boolean,Integer> map = new TreeMap<>();
+            map.put(type, number);
+            solutions.put(solution, map);
+        }else{
+            TreeMap<Boolean, Integer> object = solutions.get(solution);
+            // Vérifie si le type de cette solution n'a pas déjà été défini
+            if(object.get(type) == null){
+                object.put(type, number);
+            }else{
+                object.replace(type, object.get(type), number);
+            }
+        }
+    }
 }
