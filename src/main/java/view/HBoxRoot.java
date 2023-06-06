5c9e3257bf9a0029c587ed438d07255cf23d6c2d
Eliott-B <eliottb.info@gmail.com>
2023-06-06 14:37:41 +0200

    Create HBoxRoot and add VBox
    Co-authored-by: 444chak <444chak@gmail.com>

--- src/main/java/view/HBoxRoot.java
+++ src/main/java/view/HBoxRoot.java
@@ -1,2 +1,17 @@
-package view;public class HBoxRoot {
+package view;
+
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class HBoxRoot extends HBox {
+
+    public HBoxRoot(){
+        super();
+        // VBox des choix
+        VBox choice = new VBoxChoice();
+        // VBox des résultats
+        VBox results = new VBoxResults();
+
+        this.getChildren().addAll(choice, results);
+    }
 }
