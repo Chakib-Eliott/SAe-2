b508b47b33c96a6e491bdc4cc1da21ca842a1453
Eliott-B <eliottb.info@gmail.com>
2023-06-06 20:05:32 +0200

    Add icon

--- src/main/java/view/RPGapp.java
+++ src/main/java/view/RPGapp.java
@@ -2,9 +2,11 @@ package view;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.File;
+import java.util.Objects;
 
 public class RPGapp extends Application {
 
@@ -16,6 +18,7 @@ public class RPGapp extends Application {
 
         stage.setTitle("RPG Simulation");
         stage.centerOnScreen();
+        stage.getIcons().add(new Image("https://i.ibb.co/HY2S8P3/rpg-logo.png"));
 
         File css = new File("asset"+ File.separator+"style.css");
         scene.getStylesheets().add(css.toURI().toString());
