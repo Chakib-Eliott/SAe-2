704280b3c75e0ae4918947c55050ba214265ab37
Eliott-B <eliottb.info@gmail.com>
2023-06-06 14:37:03 +0200

    Initialization of the application
    Co-authored-by: 444chak <444chak@gmail.com>

--- src/main/java/view/RPGapp.java
+++ src/main/java/view/RPGapp.java
@@ -1,2 +1,27 @@
-package view;public class RPGapp {
+package view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class RPGapp extends Application {
+
+    public void start(Stage stage) {
+        HBox root = new HBoxRoot();
+
+        Scene scene = new Scene(root, 900, 600);
+        stage.setScene(scene);
+
+        stage.setTitle("RPG Simulation");
+        stage.centerOnScreen();
+
+        stage.setResizable(false);
+
+        stage.show();
+    }
+
+    public static void main(String [] args) {
+        Application.launch();
+    }
 }
