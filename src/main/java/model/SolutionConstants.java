f12e7274019df9f5c152e06134e408ba192d71e3
444chak <102913424+444chak@users.noreply.github.com>
2023-06-07 03:11:41 +0200

    SolutionConstants.java : add Map for solutions type (<string> to <string,integer>)

--- src/main/java/model/SolutionConstants.java
+++ src/main/java/model/SolutionConstants.java
@@ -1,4 +1,8 @@
 package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Interface des constantes pour les solutions.
  * @author Eliott B
@@ -8,4 +12,15 @@ public interface SolutionConstants {
      * Tableau des noms des solutions
      */
     String [] SOLUTIONS_NAME = {"GLOUTONNE", "SPEED RUN"};
+
+    /**
+     * Dictionnaire des types de solution
+     */
+    HashMap<String, Object[]> SOLUTIONS_TYPE = new HashMap<String, Object[]>() {{
+        put("Gloutonne efficace", new Object[] {SOLUTIONS_NAME[0], 0}); // Gloutonne efficace
+        put("Gloutonne 100%", new Object[] {SOLUTIONS_NAME[0], 1}); // Gloutonne 100%
+        put("Speedrun efficace", new Object[] {SOLUTIONS_NAME[1], 0}); // Speedrun efficace
+        put("Speedrun 100%", new Object[] {SOLUTIONS_NAME[1], 1}); // Speedrun 100%
+        put("Speedrun déplacements minimaux", new Object[] {SOLUTIONS_NAME[1], 2}); // Speedrun déplacements minimaux
+    }};
 }
