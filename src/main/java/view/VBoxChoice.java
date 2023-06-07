363d72eb850442cc425c8db253f59155b89d9e0d
444chak <102913424+444chak@users.noreply.github.com>
2023-06-07 03:13:09 +0200

    VBoxChoice.java : create table for choice of simulations

--- src/main/java/view/VBoxChoice.java
+++ src/main/java/view/VBoxChoice.java
@@ -1,17 +1,154 @@
 package view;
 
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+import javafx.util.converter.NumberStringConverter;
+import model.Solution;
+import model.SolutionConstants;
+import java.io.File;
+import java.util.Optional;
 
 public class VBoxChoice extends VBox {
 
     public VBoxChoice() {
         super();
 
+        // get all files names in "data" folder
+        File folder = new File("data");
+        File[] listOfFiles = folder.listFiles();
+
+
+        // AJOUT DE SIMULATION
+
+        ComboBox<String> scenarios = new ComboBox<>();
+        assert listOfFiles != null;
+        for (File file : listOfFiles) {
+            System.out.println(file.getName());
+            if (file.isFile()) {
+                String fileName = file.getName();
+                if (fileName.endsWith(".txt")) {
+                    // remove ".txt" from file name and replace "_" by " " and set upper case
+                    String scenarioName = fileName.substring(0, fileName.length() - 4);
+                    scenarioName = scenarioName.replace("_", " ");
+                    scenarios.getItems().add(scenarioName);
+                }
+            }
+        }
+
+        scenarios.setValue(scenarios.getItems().get(0));
+
+
+        // nombre de simulations (entier entre 1 et 20)
+        ScrollBar nbSimulations = new ScrollBar();
+        nbSimulations.setMin(1);
+        nbSimulations.setMax(20);
+        nbSimulations.setValue(1);
+        nbSimulations.setBlockIncrement(1);
+        nbSimulations.setPrefWidth(100);
+        nbSimulations.setPrefHeight(20);
+        nbSimulations.setUnitIncrement(1);
+
+        // tics scroll avec la souris (1 par 1)
+        // arrondir à l'entier le plus proche
+        nbSimulations.valueProperty().addListener((observable, oldValue, newValue) -> {
+            nbSimulations.setValue(Math.round(newValue.doubleValue()));
+        });
+
+        // affichage du nombre de simulations
+
+        Label nbSimulationsLabel = new Label();
+
+        nbSimulationsLabel.textProperty().bindBidirectional(nbSimulations.valueProperty(), new NumberStringConverter());
+        nbSimulationsLabel.setPrefWidth(100);
+        nbSimulationsLabel.setPrefHeight(20);
+
+        // tableau des simulations
+        TableView simulations = new TableView();
+
+
+        TableColumn<Simulation, String> scenarioCol = new TableColumn<>("Scenario");
+        TableColumn<Solution, String> typeCol = new TableColumn<>("Type");
+
+        scenarioCol.setCellValueFactory(new PropertyValueFactory<>("scenario"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+
+        scenarioCol.setPrefWidth(100);
+        typeCol.setPrefWidth(350);
+
+
+        simulations.getColumns().addAll(scenarioCol, typeCol);
+
+        // double clic sur une ligne
+        simulations.setRowFactory(tv -> {
+            TableRow<Simulation> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    // ouvrir une fenêtre de choix de type de solution
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    alert.setTitle("Choix du type de solution");
+                    alert.setHeaderText(null);
+                    alert.setGraphic(null);
+                    alert.setContentText("Choisissez le type de solution");
+
+                    for (String type : SolutionConstants.SOLUTIONS_TYPE.keySet()) {
+                        alert.getDialogPane().getButtonTypes().add(new ButtonType(type));
+                    }
+
+                    // masquer les boutons OK et CANCEL
+                    alert.getButtonTypes().remove(ButtonType.OK);
+                    alert.getButtonTypes().remove(ButtonType.CANCEL);
+
+
+                    Optional<ButtonType> result = alert.showAndWait();
+
+                    // si l'utilisateur a choisi un type de solution
+                    if (result.isPresent() && result.get().getButtonData() != ButtonBar.ButtonData.CANCEL_CLOSE) {
+                        // récupérer le type de solution
+                        String type = result.get().getText();
+                        // récupérer la simulation
+                        Simulation simulation = row.getItem();
+                        // modifier le type de solution
+                        simulation.setType(type);
+                        // mettre à jour la ligne
+                        simulations.refresh();
+                    }
+                }
+            });
+            return row;
+        });
+
+
+        // choix du type de solution par défaut
+        ComboBox<String> defaultType = new ComboBox<>();
+        defaultType.getItems().add(null);
+        defaultType.setPromptText("Type de solution");
+        for (String type : SolutionConstants.SOLUTIONS_TYPE.keySet()) {
+            defaultType.getItems().add(type);
+        }
+
+        // bouton d'ajout de simulation
+        Button addSimulation = new Button("Ajouter simulation");
+        addSimulation.setPrefWidth(200);
+        addSimulation.setPrefHeight(20);
+        addSimulation.setOnAction(event -> {
+            // récupérer le scénario sélectionné
+            String scenario = scenarios.getValue();
+            // récupérer le nombre de simulations
+            int nb = (int) nbSimulations.getValue();
+            // ajouter les simulations au tableau
+            for (int i = 0; i < nb; i++) {
+                if(defaultType.getValue() != null) {
+                    simulations.getItems().add(new Simulation(scenario, defaultType.getValue()));
+                }
+                else {
+                    simulations.getItems().add(new Simulation(scenario));
+                }
+            }
+        });
+
         Label title = new Label("Choix de simulations");
-        this.getChildren().add(title);
+        this.getChildren().addAll(title, scenarios, nbSimulations, nbSimulationsLabel, defaultType, addSimulation, simulations);
     }
 
     public static class Simulation {
