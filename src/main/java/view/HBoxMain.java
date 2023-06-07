885b1a612d588e5ad3efbe46252f8cd2a89769ae
444chak <102913424+444chak@users.noreply.github.com>
2023-06-07 06:09:54 +0200

    HBoxMain.java : add fields & accessors for views

--- src/main/java/view/HBoxMain.java
+++ src/main/java/view/HBoxMain.java
@@ -1,21 +1,37 @@
 package view;
 
+import controler.Controller;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class HBoxMain extends HBox {
 
+public static VBoxChoice choice = new VBoxChoice();
+public static VBoxResults results = new VBoxResults();
+public static final Controller controller = new Controller();
     public HBoxMain() {
         super();
 
         // VBox des choix
-        VBox choice = new VBoxChoice();
+        choice = new VBoxChoice();
         // VBox des résultats
-        VBox results = new VBoxResults();
+        results = new VBoxResults();
 
         choice.prefWidthProperty().bind(this.widthProperty().multiply(0.5));
         results.prefWidthProperty().bind(this.widthProperty().multiply(0.5));
 
         this.getChildren().addAll(choice, results);
+    }
+
+    public static VBoxChoice getChoice() {
+        return choice;
+    }
+
+    public static VBoxResults getResults() {
+        return results;
+    }
+
+    public static Controller getController() {
+        return controller;
     }
 }
