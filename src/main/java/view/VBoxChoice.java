c8daf083bd3276a3152f81b8e9f4fa77c0e13df8
444chak <102913424+444chak@users.noreply.github.com>
2023-06-07 03:12:51 +0200

    VBoxChoice.java : add Simuation local class for table

--- src/main/java/view/VBoxChoice.java
+++ src/main/java/view/VBoxChoice.java
@@ -13,4 +13,31 @@ public class VBoxChoice extends VBox {
         Label title = new Label("Choix de simulations");
         this.getChildren().add(title);
     }
+
+    public static class Simulation {
+        private final String scenario;
+        private String type = "Cliquez pour définir le type de solution";
+
+        public Simulation(String scenario) {
+            this.scenario = scenario;
+        }
+
+        public Simulation(String scenario, String type) {
+            this.scenario = scenario;
+            this.type = type;
+        }
+
+        public String getScenario() {
+            return scenario;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+    }
+
 }
