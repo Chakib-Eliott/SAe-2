d5081b4a881cecc21a6c53e606cdc8be59bd99a3
444chak <102913424+444chak@users.noreply.github.com>
2023-06-07 06:06:43 +0200

    add Controller.java for relation between VBoxChoice & VBoxResults

---
+++ src/main/java/controler/Controller.java
@@ -0,0 +1,27 @@
+package controler;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import view.VBoxChoice;
+import view.VBoxResults;
+
+import java.io.FileNotFoundException;
+
+public class Controller implements EventHandler {
+
+    @Override
+    public void handle(Event event) {
+        VBoxChoice choice = view.HBoxMain.getChoice();
+        VBoxResults results = view.HBoxMain.getResults();
+
+        if(event.getSource() instanceof Button) {
+            try {
+                results.TableResults.getItems().clear();
+                results.launchSimulations(choice.getSimulationsToDo());
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
