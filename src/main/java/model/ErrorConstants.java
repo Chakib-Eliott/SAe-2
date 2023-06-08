cc316216016dce95ee016b8d9b825c837c182891
Eliott-B <eliottb.info@gmail.com>
2023-06-08 08:04:11 +0200

    Fix JavaDoc

--- src/main/java/model/ErrorConstants.java
+++ src/main/java/model/ErrorConstants.java
@@ -5,7 +5,9 @@ package model;
  */
 public interface ErrorConstants {
     /**
-     * Erreur des solutions
+     * Erreur des solutions :
+     *      0 -> La solution n'existe pas
+     *      1 -> Le type n'existe pas pour cette solution
      */
     String[] ERROR_SOLUTION = {
             "ERROR -> The solution doesn't exist !",
